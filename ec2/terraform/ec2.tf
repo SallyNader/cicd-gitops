97ef4bad79c446249feef1b606442578a01f545d
solly <sally.nader.ahmed@hotmail.com>
2023-07-21 15:56:24 +0300

    Creates ec2 instance using terraform and install jenkins.

---
+++ ec2/terraform/ec2.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "cicd" {
+  ami             = var.ami
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.cicd.name]
+  tags = {
+    Name = "cicd"
+  }
+  key_name  = var.key_name
+  user_data = base64encode(file("${path.module}/bash/install-jenkins.sh"))
+
+}
+
+resource "aws_security_group" "cicd" {
+  name        = var.sg_name
+  description = var.sg_description
+
+  ingress {
+    description = "Allow ssh from everywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow http from everywhere"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
